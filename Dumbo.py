0f3be528d9753982c518024fba18cd2527657418
Marine Bodson <marineop93@gmail.com>
2023-03-31 16:27:36 +0200

    Work on Toekens

--- Dumbo.py
+++ Dumbo.py
@@ -5,7 +5,15 @@ tokens = (
     'DUMBO_MARK',
     'SEMICOLON',
     'ASSIGN',
-    'TXT'
+    'LETTERS',
+    'NUMBERS',
+    'SYMBOLS',
+    'SYMBOLS_NO_BRACKETS',
+    'PRINT',
+    'FOR',
+    'IN',
+    'DO',
+    'ENDFOR'
 )
 
 #Tokens defined by strings are added by sorting them in order of
@@ -13,10 +21,35 @@ tokens = (
 t_DUMBO_MARK = r'(\{\{)|(\}\})'
 t_SEMICOLON = r';'
 t_ASSIGN = r':='
+t_LETTERS = r'[a-zA-Z]'
+t_NUMBERS = r'\d'
+t_SYMBOLS = r'\W'
+t_SYMBOLS_NO_BRACKETS = r'[^a-zA-Z0-9{}]' #Might replace that one with a regex for simple brackets
 
 
-def t_TXT(t):
-    r'(\w|\W|[^\S])*'
+#All tokens defined by functions are added first in the same order as they appear in the lexer file
+def t_PRINT(t):
+    r'print'
+    return t
+
+
+def t_FOR(t):
+    r'for'
+    return t
+
+
+def t_IN(t):
+    r'in'
+    return t
+
+
+def t_DO(t):
+    r'do'
+    return t
+
+
+def t_ENDFOR(t):
+    r'endfor'
     return t
 
 
@@ -37,6 +70,6 @@ t_ignore = ' \t'
 #Testing
 if __name__ == "__main__":
     lexer = lex.lex()
-    lexer.input("{|} |}<  >/ ;; := \" \'")
+    lexer.input("{|}print print {{ }} |}<  >/ ;; := \" \'")
     for token in lex.lexer:
         print("line %d : %s (%s)" % (token.lineno, token.type, token.value))
